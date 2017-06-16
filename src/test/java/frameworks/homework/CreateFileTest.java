e3bbac7e2170cb20f1a0734e21e85be1e54235e5
Olena Kolesnyk <a.r.kolesnik@gmail.com>
2017-06-16 18:42:19 +0300

    Review fixes

--- src/test/java/frameworks/homework/CreateFileTest.java
+++ src/test/java/frameworks/homework/CreateFileTest.java
@@ -12,26 +12,29 @@ public class CreateFileTest extends CreateFileFixture {
     @Test(groups = "positive")
     public void shouldCreateFile() throws IOException {
         File file = new File(path + "/test.txt");
-        Assert.assertTrue(file.createNewFile(), "File was not successfully created");
+        file.createNewFile();
+        Assert.assertTrue(file.exists(), "File was not successfully created \n");
     }
 
     @Test(groups = "positive")
     public void shouldCreateFileWithLongName() throws IOException {
         File file = new File(path + "/" + RandomStringUtils.randomAlphabetic(30) + ".txt");
-        Assert.assertTrue(file.createNewFile(), "File with long name was not created");
+        file.createNewFile();
+        Assert.assertTrue(file.exists(), "File with long name was not created \n");
     }
 
     @Test(groups = "positive")
     public void shouldCreateFileWithCyrillicName() throws IOException {
         File file = new File(path + "/тест.txt");
-        Assert.assertTrue(file.createNewFile(), "File with cyrillic name was not created");
+        file.createNewFile();
+        Assert.assertTrue(file.exists(), "File with cyrillic name was not created \n");
     }
 
     @Test(groups = "negative")
     public void shouldReturnFalseWhenFileExists() throws IOException {
         File file = new File(path + "/test.txt");
         file.createNewFile();
-        Assert.assertFalse(file.createNewFile(), "File with duplicate name was created, but shouldn't be");
+        Assert.assertFalse(file.createNewFile(), "File with duplicate name was created, but shouldn't be \n");
     }
 
     @Test(groups = "negative", expectedExceptions = IOException.class)
